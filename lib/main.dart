0e8f9a4056b13ff019720bd8a903f4a6226cf880
madushan <ranasinhemadushan@gmail.com>
2022-08-02 07:37:03 +0530

    repo: pubspec yaml Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
+import 'package:covid_safe_app/configuration/app_config.dart';
 import 'package:covid_safe_app/routes/appPages.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 import 'main_binding.dart';
 
 
 void main() {
+  setEnvironment(Environment.dev);
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.transparent,
+      statusBarColor: Colors.white,
+      systemNavigationBarDividerColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+      systemNavigationBarIconBrightness: Brightness.dark));
   runApp(MyApp());
 }
 
@@ -14,10 +23,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return GetMaterialApp(
       title: 'Covid Safe App',
       theme: ThemeData(
-        primarySwatch: Colors.lightBlue,
+        primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       debugShowCheckedModeBanner: false,
