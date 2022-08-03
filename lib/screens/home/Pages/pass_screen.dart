50d0a17c8c7d8cb7fa112a3ba6ce5a59b0f438e3
madushan <ranasinhemadushan@gmail.com>
2022-08-03 18:44:37 +0530

    feat(PassScreen): Pass Screen Created

--- lib/screens/home/Pages/pass_screen.dart
+++ lib/screens/home/Pages/pass_screen.dart
@@ -12,26 +12,144 @@ class PassScreen extends StatefulWidget {
 class _PassScreenState extends State<PassScreen> {
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(
-        SystemUiOverlayStyle(
-            systemNavigationBarColor: Colors.transparent,
-            statusBarColor: Colors.white,
-            systemNavigationBarDividerColor: Colors.transparent,
-            statusBarIconBrightness: Brightness.dark,
-            systemNavigationBarIconBrightness: Brightness.dark)
-
-    );
+    final _size = MediaQuery.of(context).size;
 
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {  },
-      ),
-      body: SingleChildScrollView(child: Container(
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.grey[900],
+          child: Icon(
+            Icons.add,
+            size: 40,
+            color: Colors.yellow[800],
+          ),
+          onPressed: () {},
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+              child: Column(
+            children: [
+              Material(
+                elevation: 4,
+                shape:
+                    RoundedRectangleBorder(borderRadius: BorderRadius.only()),
+                color: Colors.grey[900],
+                child: Container(
+                  height: _size.height * 0.08,
+                  width: _size.width,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20.0, vertical: 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          flex: 2,
+                          child: Text(
+                            "Digital Pass",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: Align(
+                            alignment: Alignment.centerRight,
+                            child: Icon(
+                              Icons.qr_code,
+                              color: Colors.white,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                height: _size.height,
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  child: ListView(
+                    children: [
+                      InkWell(
+                        onTap: (){
 
+                        },
+                        child: Card(
+                          elevation: 1,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 10.0, horizontal: 15),
+                            child: Container(
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Expanded(
+                                    flex: 6,
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      mainAxisAlignment: MainAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          "QR Code For work Transportation",
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 20),
+                                        ),
+                                        Text("Last Updated : 2022-12-18 5.00 PM")
+                                      ],
+                                    ),
+                                  ),
+                                  Expanded(
+                                    flex: 4,
+                                    child: Column(
 
+                                      children: [
+                                        Align(
+                                          alignment: Alignment.centerRight,
+                                          child: Icon(
+                                            Icons.qr_code_2,
+                                            size: 60,
+                                          ),
+                                        ),
+                                        Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.end,
+                                          children: [
+                                            Icon(
+                                              Icons.arrow_circle_right_rounded,
+                                              size: 20,
+                                            ),
 
-      )),
-    );
-
+                                            Icon(
+                                              Icons.delete_rounded,
+                                              size: 20,
+                                            ),
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          )),
+        ));
   }
 }
