6f3e382cc1ef2ec71283862f958c9cfda509f179
madushan <ranasinhemadushan@gmail.com>
2022-08-04 16:51:53 +0530

    feat(binding):  Getx Bindings added

--- lib/main_binding.dart
+++ lib/main_binding.dart
@@ -1,10 +1,12 @@
+import 'package:covid_safe_app/service/Authentication/AuthService.dart';
+import 'package:covid_safe_app/service/Connection/ConnectionService.dart';
 import 'package:get/get.dart';
 
-import 'controllers/homeController.dart';
 
 class MainBinding extends Bindings {
   @override
   void dependencies() {
-    Get.put<HomeController>(HomeController());
+    Get.put<AuthService>(AuthService());
+    Get.put<ConnectivityService>(ConnectivityService());
   }
 }
