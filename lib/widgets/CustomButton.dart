1181d33c725692f5a9899ebdb11d12c7083427a6
madushan <ranasinhemadushan@gmail.com>
2022-08-04 17:15:41 +0530

    feat(Widget): CustomNumophoric Widget created

---
+++ lib/widgets/CustomButton.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+
+class CustomNumphoricWidget extends StatelessWidget {
+  const CustomNumphoricWidget({
+    Key? key, required this.onTap,
+  }) : super(key: key);
+
+
+  final Function onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+          horizontal: 20, vertical: 10),
+      child: Container(
+        child: Row(
+          children: [
+            Expanded(
+              child: Align(
+                alignment: Alignment.bottomRight,
+                child: NeumorphicButton(
+                  margin: EdgeInsets.only(top: 12),
+                  onPressed: () {
+                     onTap();
+                  },
+                  child: Icon(
+                    Icons.arrow_forward,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+                  style: NeumorphicStyle(
+                    color: Colors.black,
+                    shape: NeumorphicShape.flat,
+                    boxShape: NeumorphicBoxShape.circle(),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButtonWidget2 extends StatelessWidget {
+  const CustomButtonWidget2({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Container(
+        height: 40,
+        width: 80,
+        child: Row(
+          crossAxisAlignment:
+          CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 7,
+              child: Center(
+                child: Text(
+                  "REGISTER",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: "Montserrat",
+                      fontWeight: FontWeight.w600),
+                ),
+              ),
+            ),
+          ],
+        ),
+        decoration: BoxDecoration(
+            color: Color(0xFF252525),
+            borderRadius: BorderRadius.circular(10)),
+      ),
+    );
+  }
+}
