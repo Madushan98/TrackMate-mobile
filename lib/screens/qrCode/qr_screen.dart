50a68a8fd7c013878201c373ac6ee0519b25d2c4
madushan <ranasinhemadushan@gmail.com>
2022-08-04 16:43:05 +0530

    feat(QRScreen): QRScreen created

--- lib/screens/qrCode/qr_screen.dart
+++ lib/screens/qrCode/qr_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class QrCodeScreen extends StatefulWidget {
-  const QrCodeScreen({Key? key}) : super(key: key);
+  const QrCodeScreen({Key? key, required this.id}) : super(key: key);
+
+  final String id;
 
   @override
   State<QrCodeScreen> createState() => _QrCodeScreenState();
@@ -10,6 +14,19 @@ class QrCodeScreen extends StatefulWidget {
 class _QrCodeScreenState extends State<QrCodeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey[900],
+        title: Text("Generated QR "),
+      ),
+      body: Center(
+        child: QrImage(
+          data: "Madushan Ranasinge",
+          version: QrVersions.auto,
+          size: 250.0,
+          gapless: false,
+        ),
+      ),
+    );
   }
 }
