8f37eb6b23f6c739e302a94ab63714dab422f75a
madushan <ranasinhemadushan@gmail.com>
2022-08-04 16:53:05 +0530

    feat(dashBord): dashbord and homepage updated

--- lib/screens/home/Pages/dashBord_screen.dart
+++ lib/screens/home/Pages/dashBord_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../../../widgets/profileAvatar.dart';
 
@@ -11,33 +12,122 @@ class DashBord extends StatefulWidget {
 }
 
 class _DashBordState extends State<DashBord> {
-
-
   String imageUrl =
       'https://img.freepik.com/free-photo/handsome-young-businessman-shirt-eyeglasses_85574-6228.jpg?size=626&ext=jpg';
 
-
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(child: Container(
-      child: Column(
-        children: [
-          Container(
-            child: Row(
-              children: [
-                Expanded(
-                  child: Text(
-                    "MAdushan Ranasinghe"
+
+    final _size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SingleChildScrollView(
+          child: Container(
+        child: Column(
+          children: [
+            Material(
+              elevation: 3,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(25),
+                      bottomRight: Radius.circular(25))),
+              color: Colors.grey[900],
+              child: Container(
+                decoration: BoxDecoration(),
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 10.0,bottom: 20),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 20.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Expanded(
+                              flex: 10,
+                              child: Text(
+                                "Madushan",
+                                style: TextStyle(
+                                    fontSize: _size.height * 0.02,
+                                    color: Colors.white),
+                              ),
+                            ),
+                            Expanded(
+                                flex: 2,
+                                child: ProfileAvatar(
+                                  imageUrl: imageUrl,
+                                  size: 20,
+                                )),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 20.0),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Covid-19",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: _size.height * 0.03),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      Container(
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 20.0),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Expanded(
+                                flex: 6,
+                                child: Text(
+                                  "Your Account is not yet Activated",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                              ),
+                              Expanded(
+                                flex: 2,
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    color: Colors.white,
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Center(
+                                      child: Text(
+                                        "Activate",
+                                        style: TextStyle(
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.w600),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Expanded(flex: 1, child: Container())
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
                   ),
                 ),
-                Container(
-                  child: ProfileAvatar(imageUrl: imageUrl,size: 20,),
-                )
-              ],
+              ),
             ),
-          )
-        ],
-      ),
-    ));
+          ],
+        ),
+      )),
+    );
   }
 }
---
+++ lib/screens/home/home_page.dart
@@ -0,0 +1,127 @@
+import 'package:covid_safe_app/controllers/screenControllers/dashBoardController.dart';
+import 'package:covid_safe_app/screens/home/Pages/account_screen.dart';
+import 'package:covid_safe_app/screens/home/Pages/dashBord_screen.dart';
+import 'package:covid_safe_app/screens/home/Pages/location_screen.dart';
+import 'package:covid_safe_app/screens/home/Pages/pass_screen.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  var selectedIndex = 0;
+
+  final DashBoardController dashBoardController =
+      Get.put(DashBoardController());
+
+  onItemTap(int value) async {
+    setState(() {
+      selectedIndex = value;
+    });
+  }
+
+  static List<Widget> _pages = <Widget>[
+    DashBord(),
+    LocationScreen(),
+    DashBord(),
+    PassScreen(),
+    AccountScreen()
+
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+        systemNavigationBarColor: Colors.transparent,
+        statusBarColor: Colors.grey[900],
+        systemNavigationBarDividerColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.light,
+        systemNavigationBarIconBrightness: Brightness.dark));
+
+    return SafeArea(
+      child: Scaffold(
+
+        backgroundColor: Colors.white,
+
+        bottomNavigationBar: Container(
+          decoration: BoxDecoration(
+
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black,
+                    offset: Offset(0, 4),
+                    blurRadius: 4)
+              ]
+          ),
+          child: Material(
+
+            color:Colors.white,
+            child: BottomNavigationBar(
+
+              items: const <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.dashboard,
+                    size: 25,
+                  ),
+                  label: 'DashBord',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.location_on_rounded,
+                    size: 25,
+                  ),
+                  label: 'Map',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.notifications_rounded,
+                    size: 25,
+                  ),
+                  label: "Alert",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.qr_code_2_rounded,
+                    size: 25,
+                  ),
+                  label: 'Pass',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.person,
+                    size: 25,
+                  ),
+                  label: 'Account',
+                ),
+              ],
+              type: BottomNavigationBarType.shifting,
+              unselectedItemColor: Colors.black,
+              selectedIconTheme:
+                  IconThemeData(color: Colors.yellow[800], size: 25),
+              selectedItemColor: Colors.yellow[800],
+              selectedLabelStyle: TextStyle(fontWeight: FontWeight.w400),
+              currentIndex: selectedIndex,
+              backgroundColor: Colors.transparent,
+              elevation: 20,
+              showSelectedLabels: true,
+              showUnselectedLabels: true,
+              onTap: (value) {
+                onItemTap(value);
+              },
+            ),
+          ),
+        ),
+        body:  _pages.elementAt(selectedIndex),
+      ),
+    );
+  }
+}
