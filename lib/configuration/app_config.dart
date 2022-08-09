4765d1cdc13ad7504aeed8ab1b3cc19c5fd49c5f
madushan <ranasinhemadushan@gmail.com>
2022-08-09 14:08:59 +0530

    feat(config): auth service url updated

--- lib/configuration/app_config.dart
+++ lib/configuration/app_config.dart
@@ -1,5 +1,5 @@
 const _baseUrl = "baseUrl";
-const _userService = "user-service";
+const _authService = "user-service";
 
 enum Environment { dev, stage, prod }
 
@@ -24,20 +24,20 @@ dynamic get apiBaseUrl {
 }
 
 dynamic get userService {
-  return _config[_userService];
+  return _config[_authService];
 }
 
 Map<String, dynamic> devConstants = {
   _baseUrl: "https://localhost:7005/api/v1/",
-  _userService: "https://l6y2icvu11.execute-api.us-west-2.amazonaws.com/api/v1/"
+  _authService: "https://5jr99q3aq7.execute-api.us-west-2.amazonaws.com/api/v1/"
 };
 
 Map<String, dynamic> stageConstants = {
   _baseUrl: "https://localhost:7005/api/v1/",
-  _userService:"https://l6y2icvu11.execute-api.us-west-2.amazonaws.com/api/v1/"
+  _authService: "https://5jr99q3aq7.execute-api.us-west-2.amazonaws.com/api/v1/"
 };
 
 Map<String, dynamic> prodConstants = {
   _baseUrl: "https://localhost:7005/api/v1/",
-  _userService: "https://l6y2icvu11.execute-api.us-west-2.amazonaws.com/api/v1/"
+  _authService: "https://5jr99q3aq7.execute-api.us-west-2.amazonaws.com/api/v1/"
 };
