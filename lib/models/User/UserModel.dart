9ef68f41d4c53db5a3d21f13ada90d44fb22941b
madushan <ranasinhemadushan@gmail.com>
2022-08-19 18:21:53 +0530

    fix: user model updated

--- lib/models/User/UserModel.dart
+++ lib/models/User/UserModel.dart
@@ -1,94 +1,96 @@
-/// userId : "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+/// id : "3fa85f64-5717-4562-b3fc-2c963f66afa6"
 /// nationalId : "string"
-/// firstName : "string"
-/// lastName : "string"
-/// birthDate : "2022-08-04T08:02:44.816Z"
-/// primaryContactNumber : "string"
+/// fullName : "string"
 /// town : "string"
 /// district : "string"
 /// isVertified : true
+/// gender : "string"
+/// location : "string"
+/// birthDate : "2022-08-17T18:17:40.969Z"
+/// primaryContactNumber : "string"
+/// emergencyContactNumber : "string"
+/// userType : "string"
 /// permissions : [0]
 /// roles : ["string"]
 
 class User {
   User({
-    this.userId,
-    this.nationalId,
-    this.firstName,
-    this.lastName,
-    this.birthDate,
-    this.primaryContactNumber,
-    this.town,
-    this.district,
-    this.isVertified,
-    this.permissions,
-    this.roles,
-  });
+
+      this.nationalId, 
+      this.fullName, 
+      this.town, 
+      this.district, 
+      this.isVertified, 
+      this.gender, 
+      this.location,
+      this.primaryContactNumber, 
+      this.emergencyContactNumber, 
+      this.userType,});
 
   User.fromJson(dynamic json) {
-    userId = json['userId'];
+
     nationalId = json['nationalId'];
-    firstName = json['firstName'];
-    lastName = json['lastName'];
-    birthDate = json['birthDate'];
-    primaryContactNumber = json['primaryContactNumber'];
+    fullName = json['fullName'];
     town = json['town'];
     district = json['district'];
     isVertified = json['isVertified'];
-    permissions =
-        json['permissions'] != null ? json['permissions'].cast<int>() : [];
-    roles = json['roles'] != null ? json['roles'].cast<String>() : [];
+    gender = json['gender'];
+    location = json['location'];
+    primaryContactNumber = json['primaryContactNumber'];
+    emergencyContactNumber = json['emergencyContactNumber'];
+    userType = json['userType'];
   }
-  String? userId;
+
   String? nationalId;
-  String? firstName;
-  String? lastName;
-  String? birthDate;
-  String? primaryContactNumber;
+  String? fullName;
   String? town;
   String? district;
   bool? isVertified;
-  List<int>? permissions;
-  List<String>? roles;
-  User copyWith({
-    String? userId,
-    String? nationalId,
-    String? firstName,
-    String? lastName,
-    String? birthDate,
-    String? primaryContactNumber,
-    String? town,
-    String? district,
-    bool? isVertified,
-    List<int>? permissions,
-    List<String>? roles,
-  }) =>
-      User(
-        userId: userId ?? this.userId,
-        nationalId: nationalId ?? this.nationalId,
-        firstName: firstName ?? this.firstName,
-        lastName: lastName ?? this.lastName,
-        birthDate: birthDate ?? this.birthDate,
-        primaryContactNumber: primaryContactNumber ?? this.primaryContactNumber,
-        town: town ?? this.town,
-        district: district ?? this.district,
-        isVertified: isVertified ?? this.isVertified,
-        permissions: permissions ?? this.permissions,
-        roles: roles ?? this.roles,
-      );
+  String? gender;
+  String? location;
+  String? birthDate;
+  String? primaryContactNumber;
+  String? emergencyContactNumber;
+  String? userType;
+
+User copyWith({  String? id,
+  String? nationalId,
+  String? fullName,
+  String? town,
+  String? district,
+  bool? isVertified,
+  String? gender,
+  String? location,
+  String? primaryContactNumber,
+  String? emergencyContactNumber,
+  String? userType,
+
+}) => User(
+  nationalId: nationalId ?? this.nationalId,
+  fullName: fullName ?? this.fullName,
+  town: town ?? this.town,
+  district: district ?? this.district,
+  isVertified: isVertified ?? this.isVertified,
+  gender: gender ?? this.gender,
+  location: location ?? this.location,
+  primaryContactNumber: primaryContactNumber ?? this.primaryContactNumber,
+  emergencyContactNumber: emergencyContactNumber ?? this.emergencyContactNumber,
+  userType: userType ?? this.userType,
+
+);
   Map<String, dynamic> toJson() {
     final map = <String, dynamic>{};
-    map['userId'] = userId;
     map['nationalId'] = nationalId;
-    map['firstName'] = firstName;
-    map['lastName'] = lastName;
-    map['birthDate'] = birthDate;
-    map['primaryContactNumber'] = primaryContactNumber;
+    map['fullName'] = fullName;
     map['town'] = town;
     map['district'] = district;
     map['isVertified'] = isVertified;
-    map['permissions'] = permissions;
-    map['roles'] = roles;
+    map['gender'] = gender;
+    map['location'] = location;
+    map['primaryContactNumber'] = primaryContactNumber;
+    map['emergencyContactNumber'] = emergencyContactNumber;
+    map['userType'] = userType;
     return map;
   }
+
 }
