509c3ab8ceccae01a35f5550be05ae0c8f4c407a
madushan <ranasinhemadushan@gmail.com>
2022-08-19 18:25:04 +0530

    feat: auth service updated

--- lib/configuration/app_constants.dart
+++ lib/configuration/app_constants.dart
@@ -3,6 +3,7 @@
 class Constants {
 
   static const String authToken =  "auth-token" ;
+  static const String userDetails = "User";
   static const String personImage = "assets/images/user/person.png";
 
 }
