0265ac7bab7b1655266bf34758e620fa0aaa14f4
madushan <ranasinhemadushan@gmail.com>
2022-12-18 17:52:29 +0530

    feat(Pass): Pass screen tab created

--- lib/screens/home/Pages/pass_screen.dart
+++ lib/screens/home/Pages/pass_screen.dart
@@ -1,5 +1,6 @@
 import 'package:covid_safe_app/controllers/passControllers/pass-manger_controller.dart';
 import 'package:covid_safe_app/helper/dateTime.dart';
+import 'package:covid_safe_app/models/PassData/PassModel.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -7,7 +8,6 @@ import 'package:get/get_core/src/get_main.dart';
 import '../../../routes/appPages.dart';
 import '../../qrCode/qr_screen.dart';
 
-// Pass Screen  Shows All  Passes of the user
 class PassScreen extends StatefulWidget {
   const PassScreen({Key? key}) : super(key: key);
 
@@ -43,165 +43,218 @@ class _PassScreenState extends State<PassScreen> {
             },
           ),
         ),
+        appBar: AppBar(
+          elevation: 4,
+          title: Text(
+            'Passes',
+            style: TextStyle(
+                color: Colors.white, fontSize: 20, fontWeight: FontWeight.w500),
+          ),
+          backgroundColor: Colors.grey[900],
+          actions: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Icon(
+                Icons.qr_code,
+                color: Colors.white,
+              ),
+            )
+          ],
+        ),
+
         body: SingleChildScrollView(
           child: Obx(() {
-            return RefreshIndicator(
-              onRefresh: () async {
-                _passController.getAllPasses();
-              },
-              child: Container(
-                  child: Column(
-                children: [
-                  Material(
-                    elevation: 4,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.only()),
-                    color: Colors.grey[900],
-                    child: Container(
-                      height: _size.height * 0.1,
-                      width: _size.width,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 20.0, vertical: 10),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Expanded(
-                              flex: 2,
-                              child: Text(
-                                "Digital Pass",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.w500),
-                              ),
-                            ),
-                            Expanded(
-                              flex: 1,
-                              child: Align(
-                                alignment: Alignment.centerRight,
-                                child: Icon(
-                                  Icons.qr_code,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                  _passController.isLoading.value
-                      ? Container(
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 30, vertical: 20),
-                            child: Container(
-                              height: _size.height * 0.8,
-                              width: _size.height,
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  CircularProgressIndicator(
-                                    color: Colors.grey[900],
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        )
-                      : Container(
-                          height: _size.height,
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 10, vertical: 10),
-                            child: ListView.builder(
-                                itemCount: _passController.passList.length,
-                                itemBuilder: (BuildContext context, int index) {
-                                  var pass = _passController.passList[index];
-                                  return InkWell(
-                                    onTap: () {
-                                      Get.toNamed(Routes.QrCodeScreen,
-                                          arguments: QrCodeScreen(
-                                            passId: pass.id!,
-                                          ));
+            return Container(
+                 height: _size.height,
+                 child: Padding(
+                   padding: const EdgeInsets.symmetric(
+                       horizontal: 10, vertical: 10),
+                   child: _passController.isLoading.value
+                       ? Container(
+                     child: Padding(
+                       padding: const EdgeInsets.symmetric(
+                           horizontal: 30, vertical: 20),
+                       child: Container(
+                         height: _size.height * 0.8,
+                         width: _size.height,
+                         child: Row(
+                           mainAxisAlignment: MainAxisAlignment.center,
+                           children: [
+                             CircularProgressIndicator(
+                               color: Colors.grey[900],
+                             ),
+                           ],
+                         ),
+                       ),
+                     ),
+                   )
+                       : DefaultTabController(
+                     length: 2,
+                     child: Column(
+                       mainAxisSize: MainAxisSize.min,
+                       children: <Widget>[
+                         Container(
+                           child: TabBar(
+                               labelColor: Colors.black,
+                               indicatorColor: Colors.grey[500],
+                               indicatorWeight: 4, tabs: [
+                             Tab(
+
+                               child: Text(
+                                 "Accepted Passes",
+                                 style: TextStyle(
+                                     color: Colors.black54,
+                                     fontSize: 16,
+                                     fontWeight: FontWeight.w700),
+                               ),
+                             ),
+                             Tab(
+                               child: Text(
+                                 "Pending Passes",
+                                 style: TextStyle(
+                                     color: Colors.black54,
+                                     fontSize: 16,
+                                     fontWeight: FontWeight.w700),
+                               ),
+                             ),
+                           ]),
+                         ),
+                         Container(
+                           child: Flexible(
+                             child: Container(
+                               child: TabBarView(children: [
+                                 RefreshIndicator(
+                                   onRefresh: () async {
+                                     _passController.getAllPasses();
+                                   },
+                                   child: Container(
+                                       height: _size.height,
+                                       child: Padding(
+                                         padding: const EdgeInsets.symmetric(
+                                             horizontal: 10, vertical: 10),
+                                         child: ListView.builder(
+                                             itemCount: _passController.passList.length,
+                                             itemBuilder: (BuildContext context, int index) {
+                                               var pass = _passController.passList[index];
+                                               return PassCardWidget(pass: pass);
+                                             }),
+                                       )),
+                                 ),
+                                 RefreshIndicator(
+                                    onRefresh: () async {
+                                      _passController.getAllPasses();
                                     },
-                                    child: Card(
-                                      elevation: 1,
-                                      shape: RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(8),
-                                      ),
-                                      child: Padding(
-                                        padding: const EdgeInsets.symmetric(
-                                            vertical: 10.0, horizontal: 15),
-                                        child: Container(
-                                          child: Row(
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.center,
-                                            children: [
-                                              Expanded(
-                                                flex: 6,
-                                                child: Column(
-                                                  crossAxisAlignment:
-                                                      CrossAxisAlignment.start,
-                                                  mainAxisAlignment:
-                                                      MainAxisAlignment.start,
-                                                  children: [
-                                                    Text(
-                                                      "${pass.passCategory}",
-                                                      style: TextStyle(
-                                                          fontWeight:
-                                                              FontWeight.bold,
-                                                          fontSize: 20),
-                                                    ),
-                                                    Text(
-                                                        "Generated Time : ${getDateTimeString(pass.generatedDateTime!)}")
-                                                  ],
-                                                ),
-                                              ),
-                                              Expanded(
-                                                flex: 4,
-                                                child: Column(
-                                                  children: [
-                                                    Align(
-                                                      alignment:
-                                                          Alignment.centerRight,
-                                                      child: Icon(
-                                                        Icons.qr_code_2,
-                                                        size: 60,
-                                                      ),
-                                                    ),
-                                                    Row(
-                                                      mainAxisAlignment:
-                                                          MainAxisAlignment.end,
-                                                      children: [
-                                                        Icon(
-                                                          Icons
-                                                              .arrow_circle_right_rounded,
-                                                          size: 20,
-                                                        ),
-                                                        Icon(
-                                                          Icons.delete_rounded,
-                                                          size: 20,
-                                                        ),
-                                                      ],
-                                                    )
-                                                  ],
-                                                ),
-                                              ),
-                                            ],
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                  );
-                                }),
-                          )),
-                ],
-              )),
-            );
+                                   child: Container(
+                                       height: _size.height,
+                                       child: Padding(
+                                         padding: const EdgeInsets.symmetric(
+                                             horizontal: 10, vertical: 10),
+                                         child: ListView.builder(
+                                             itemCount: _passController.passList.length,
+                                             itemBuilder: (BuildContext context, int index) {
+                                               var pass = _passController.passList[index];
+                                               return PassCardWidget(pass: pass);
+                                             }),
+                                       )),
+                                 ),
+                               ]),
+                             ),
+                           ),
+                         ),
+                       ],
+                     ),
+                   ),
+                 ));
           }),
         ));
+  }
+}
+
+class PassCardWidget extends StatelessWidget {
+  const PassCardWidget({
+    Key? key,
+    required this.pass,
+  }) : super(key: key);
+
+  final PassModel pass;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Get.toNamed(Routes.QrCodeScreen,
+            arguments: QrCodeScreen(
+              passId: pass.id!,
+            ));
+      },
+      child: Card(
+        elevation: 1,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+              vertical: 10.0, horizontal: 15),
+          child: Container(
+            child: Row(
+              crossAxisAlignment:
+              CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  flex: 6,
+                  child: Column(
+                    crossAxisAlignment:
+                    CrossAxisAlignment.start,
+                    mainAxisAlignment:
+                    MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        "${pass.passCategory}",
+                        style: TextStyle(
+                            fontWeight:
+                            FontWeight.bold,
+                            fontSize: 20),
+                      ),
+                      Text(
+                          "Generated Time : ${getDateTimeString(pass.generatedDateTime!)}")
+                    ],
+                  ),
+                ),
+                Expanded(
+                  flex: 4,
+                  child: Column(
+                    children: [
+                      Align(
+                        alignment:
+                        Alignment.centerRight,
+                        child: Icon(
+                          Icons.qr_code_2,
+                          size: 60,
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment:
+                        MainAxisAlignment.end,
+                        children: [
+                          Icon(
+                            Icons
+                                .arrow_circle_right_rounded,
+                            size: 20,
+                          ),
+                          Icon(
+                            Icons.delete_rounded,
+                            size: 20,
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
