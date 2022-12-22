55767820da465c17eb9f789acb335cf217c61610
madushan <ranasinhemadushan@gmail.com>
2022-12-23 03:38:36 +0530

    feat: usermodel updated

--- lib/models/User/UserModel.dart
+++ lib/models/User/UserModel.dart
@@ -1,34 +1,39 @@
-/// id : "3fa85f64-5717-4562-b3fc-2c963f66afa6"
-/// nationalId : "string"
-/// fullName : "string"
-/// town : "string"
-/// district : "string"
-/// isVertified : true
-/// gender : "string"
-/// location : "string"
-/// birthDate : "2022-08-17T18:17:40.969Z"
-/// primaryContactNumber : "string"
-/// emergencyContactNumber : "string"
-/// userType : "string"
-/// permissions : [0]
-/// roles : ["string"]
+/// id : "11111111-1111-1111-1111-111111112213"
+/// nationalId : "9843211234"
+/// fullName : "Achila Sandeep"
+/// town : null
+/// district : null
+/// isVertified : "Pending"
+/// gender : null
+/// location : null
+/// birthDate : "0001-01-01T00:00:00"
+/// primaryContactNumber : null
+/// emergencyContactNumber : null
+/// userType : "User"
+/// permissions : null
+/// roles : null
+/// vaccinationData : []
 
 class User {
   User({
-      this.id,
+      this.id, 
       this.nationalId, 
       this.fullName, 
       this.town, 
       this.district, 
       this.isVertified, 
       this.gender, 
-      this.location,
+      this.location, 
+      this.birthDate, 
       this.primaryContactNumber, 
       this.emergencyContactNumber, 
-      this.userType,});
+      this.userType, 
+      this.permissions, 
+      this.roles, 
+      });
 
   User.fromJson(dynamic json) {
-    id = json["id"];
+    id = json['id'];
     nationalId = json['nationalId'];
     fullName = json['fullName'];
     town = json['town'];
@@ -36,38 +41,45 @@ class User {
     isVertified = json['isVertified'];
     gender = json['gender'];
     location = json['location'];
+    birthDate = json['birthDate'];
     primaryContactNumber = json['primaryContactNumber'];
     emergencyContactNumber = json['emergencyContactNumber'];
     userType = json['userType'];
-  }
+    permissions = json['permissions'];
+    roles = json['roles'];
 
+  }
   String? id;
   String? nationalId;
   String? fullName;
-  String? town;
-  String? district;
-  bool? isVertified;
-  String? gender;
-  String? location;
+  dynamic town;
+  dynamic district;
+  String? isVertified;
+  dynamic gender;
+  dynamic location;
   String? birthDate;
-  String? primaryContactNumber;
-  String? emergencyContactNumber;
+  dynamic primaryContactNumber;
+  dynamic emergencyContactNumber;
   String? userType;
+  dynamic permissions;
+  dynamic roles;
 
 User copyWith({  String? id,
   String? nationalId,
   String? fullName,
-  String? town,
-  String? district,
-  bool? isVertified,
-  String? gender,
-  String? location,
-  String? primaryContactNumber,
-  String? emergencyContactNumber,
+  dynamic town,
+  dynamic district,
+  String? isVertified,
+  dynamic gender,
+  dynamic location,
+  String? birthDate,
+  dynamic primaryContactNumber,
+  dynamic emergencyContactNumber,
   String? userType,
-
-}) => User(
-  id: id ?? this.id,
+  dynamic permissions,
+  dynamic roles,
+  List<dynamic>? vaccinationData,
+}) => User(  id: id ?? this.id,
   nationalId: nationalId ?? this.nationalId,
   fullName: fullName ?? this.fullName,
   town: town ?? this.town,
@@ -75,9 +87,12 @@ User copyWith({  String? id,
   isVertified: isVertified ?? this.isVertified,
   gender: gender ?? this.gender,
   location: location ?? this.location,
+  birthDate: birthDate ?? this.birthDate,
   primaryContactNumber: primaryContactNumber ?? this.primaryContactNumber,
   emergencyContactNumber: emergencyContactNumber ?? this.emergencyContactNumber,
   userType: userType ?? this.userType,
+  permissions: permissions ?? this.permissions,
+  roles: roles ?? this.roles,
 
 );
   Map<String, dynamic> toJson() {
@@ -90,9 +105,12 @@ User copyWith({  String? id,
     map['isVertified'] = isVertified;
     map['gender'] = gender;
     map['location'] = location;
+    map['birthDate'] = birthDate;
     map['primaryContactNumber'] = primaryContactNumber;
     map['emergencyContactNumber'] = emergencyContactNumber;
     map['userType'] = userType;
+    map['permissions'] = permissions;
+    map['roles'] = roles;
     return map;
   }
 
