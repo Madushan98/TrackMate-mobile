f9321a125c91c7dafee66afc694a77d195e7183f
madushan <ranasinhemadushan@gmail.com>
2022-12-23 03:34:18 +0530

    feat: configurations added

--- lib/configuration/app_constants.dart
+++ lib/configuration/app_constants.dart
@@ -6,3 +6,12 @@ class Constants {
   static const String imageUrl =
       'https://img.freepik.com/free-photo/handsome-young-businessman-shirt-eyeglasses_85574-6228.jpg?size=626&ext=jpg';
 }
+
+
+
+
+class VerificationStatus {
+  static const String verified = "Verified";
+  static const String notVerified = "NotVerified";
+  static const String pending = "Pending";
+}
