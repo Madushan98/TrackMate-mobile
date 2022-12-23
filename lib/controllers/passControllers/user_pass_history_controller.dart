ba719a0f364ae447d5b2dfefd4d01b465b5d01e5
madushan <ranasinhemadushan@gmail.com>
2022-12-24 00:04:50 +0530

    feat(PassLog): pass log date picker created

--- lib/controllers/passControllers/user_pass_history_controller.dart
+++ lib/controllers/passControllers/user_pass_history_controller.dart
@@ -1,4 +1,6 @@
 import 'package:covid_safe_app/service/Pass/PassService.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../../models/PassData/PassHistoryModel.dart';
@@ -8,9 +10,10 @@ import '../baseController.dart';
 class UserPassHistoryController extends GetxController with BaseController {
   var isLoading = false.obs;
   var passLogHistory = RxList<PassHistoryModel>();
+  var selectedDateLogHistory = RxList<PassHistoryModel>();
   var _passService = Get.find<PassService>();
   var _authService = Get.find<AuthService>();
-
+  var selectedDate = DateTime.now().obs;
   @override
   void onInit() {
     super.onInit();
@@ -25,7 +28,35 @@ class UserPassHistoryController extends GetxController with BaseController {
     passData.forEach((element) {
       passLogHistory.add(element);
     });
+    getSelectedDateLogHistory();
     isLoading.value = false;
     update();
   }
+
+
+  selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: selectedDate.value,
+      firstDate: DateTime(2020),
+      lastDate: DateTime(2030),
+    );
+    if (picked != null && picked != selectedDate){
+      selectedDate.value = picked;
+      getSelectedDateLogHistory();
+    }
+    update();
+  }
+
+  getSelectedDateLogHistory() {
+    selectedDateLogHistory.clear();
+    passLogHistory.forEach((element) {
+      if (element.getDateOnly()== selectedDate.value.toString().split(' ')[0]) {
+        selectedDateLogHistory.add(element);
+      }
+    });
+    update();
+  }
+
+
 }
